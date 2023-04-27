fb1e7e0b93a829f5034009217d29511b665fde99
Jetbek <131946043+Jetbek@users.noreply.github.com>
2023-04-27 23:23:05 +0500

    Delete templateProject directory
    good

--- templateProject/src/main/java/com/example/templateproject/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.templateproject;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface Repository extends JpaRepository<Entity,Integer> {
-    Optional<Entity> findByUsername(String username);
-}
